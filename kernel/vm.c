b1dfdc95999be53280097483c92f9612a346c6a0
Yizhen Zhang <zhangyizhenyizen@gmail.com>
2025-01-06 09:01:32 -0800

    labpagetable-print-pagetable

--- kernel/vm.c
+++ kernel/vm.c
@@ -185,7 +185,35 @@ uvmunmap(pagetable_t pagetable, uint64 va, uint64 npages, int do_free)
     *pte = 0;
   }
 }
+#ifdef LAB_PGTBL
+void vmprint_helper(pagetable_t pagetable, int level) 
+{
+  // there are 2^9 = 512 PTEs in a page table.
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if((pte & PTE_V) && (pte & (PTE_R|PTE_W|PTE_X)) == 0){
+      // this PTE points to a lower-level page table.
+      uint64 child = PTE2PA(pte);
+      for (int j = 1; j <= level; j++) {
+        printf(".. ");
+      }
+      printf(".. %d: pte %p pa %p\n", i, pte, child);
+      vmprint_helper((pagetable_t)child, level + 1);
+    } else if(pte & PTE_V){
+      uint64 child = PTE2PA(pte);
+      for (int j = 1; j <= level; j++) {
+        printf(".. ");
+      }
+      printf(".. %d: pte %p pa %p\n", i, pte, child);
+  }
+  }
+}
 
+void vmprint(pagetable_t pagetable) {
+  printf("page table %p\n", pagetable);
+  vmprint_helper(pagetable, 0);
+}
+#endif
 // create an empty user page table.
 // returns 0 if out of memory.
 pagetable_t
