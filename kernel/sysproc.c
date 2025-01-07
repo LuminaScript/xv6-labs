95771fa0891f7166f16549dae71901a3dbe44852
Yizhen Zhang <zhangyizhenyizen@gmail.com>
2025-01-07 07:35:56 -0800

    labpagetable-page-accessed

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -7,6 +7,12 @@
 #include "spinlock.h"
 #include "proc.h"
 
+#ifdef LAB_PGTBL
+#define MAX_PGACCESS 64
+extern pte_t *
+walk(pagetable_t pagetable, uint64 va, int alloc);
+#endif
+
 uint64
 sys_exit(void)
 {
@@ -81,6 +87,40 @@ int
 sys_pgaccess(void)
 {
   // lab pgtbl: your code here.
+  // get input parameter from user
+  uint64 va;
+  int n;
+  uint64 buf;
+
+  if(argaddr(0, &va) < 0)
+    return -1;
+  if(argint(1, &n) < 0)
+    return -1;
+  if(argaddr(2, &buf) < 0)
+    return -1;
+
+  if(va >= MAXVA)
+    panic("walk");
+  if (n <= 0)
+    return -1;
+  if (n > MAX_PGACCESS)
+    n = MAX_PGACCESS;    // restrict to a “safe” maximum
+
+  uint64 result = 0;
+  struct proc *p = myproc();
+  pagetable_t pt = p->pagetable;
+
+  for (int i = 0; i < n; i++) {
+    uint64 curr_va = va + ((uint64)i)*PGSIZE;
+    pte_t *pte = walk(pt, curr_va, 0);
+
+    if (pte && (*pte & PTE_V) && (*pte & PTE_A)) result |= (1ULL << i);
+    *pte &= ~PTE_A;
+  }
+
+  if (copyout(pt, buf, (char *)&result, sizeof(result)) < 0)
+    return -1;
+    
   return 0;
 }
 #endif
